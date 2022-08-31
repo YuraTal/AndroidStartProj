b883cc5f30acc14b206586c8e532c6c39bdfe4a0
Yuriy Talabaev <110741536+YuraTal@users.noreply.github.com>
2022-08-31 22:57:51 +0300

    We have learned linear All View and started layouts

--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -3,6 +3,7 @@ package ru.synergy.androidstartproj;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -13,7 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TextView textView = (TextView) findViewById(R.id.header);
+
+        TextView textView = (TextView) findViewById(R.id.normal);
+        textView.setText("Text from Java code");
+        textView.setTextColor(Color.RED);
 //
 //        textView.setText("Hello from Java");
 
