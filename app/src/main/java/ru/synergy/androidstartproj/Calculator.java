d90b05100686f4174b53c762a38c310469f35bc6
Yuriy Talabaev <110741536+YuraTal@users.noreply.github.com>
2022-09-08 18:41:32 +0300

    Lifecycle of activity

--- app/src/main/java/ru/synergy/androidstartproj/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproj/Calculator.java
@@ -14,9 +14,13 @@ import android.widget.Toast;
 public class Calculator extends AppCompatActivity {
 
     private static final String LogcatTag = "CALCULATOR_ACTIVITY";
+    private static final String LifecycleTag = "LIFECYCLE";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LifecycleTag, "I'm onCreate(), and I'm started");
         setContentView(R.layout.activity_calculator);
 
         final Button calculate = (Button) findViewById(R.id.calc);
@@ -32,6 +36,37 @@ public class Calculator extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LifecycleTag, "I'm onStart(), and I'm started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LifecycleTag, "I'm onStop(), and I'm started");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LifecycleTag, "I'm onDestroy(), and I'm started");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LifecycleTag, "I'm onPause(), and I'm started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LifecycleTag, "I'm onResume(), and I'm started");
+    }
+
     private void calculateAnswer() {
         EditText numOne = (EditText) findViewById(R.id.editTextNumberDecimal);
         EditText numTwo = (EditText) findViewById(R.id.editTextNumberDecimal2);
@@ -44,9 +79,16 @@ public class Calculator extends AppCompatActivity {
         TextView answer = (TextView) findViewById(R.id.result);
 
         Log.d(LogcatTag, "all views have been founded");
+        float numone=0;
+        float numtwo=0;
+        String num1 = numOne.getText().toString();
+        String num2 = numTwo.getText().toString();
 
-        float numone = Integer.parseInt(numOne.getText().toString());
-        float numtwo = Integer.parseInt(numTwo.getText().toString());
+        if(num1.equals("") && num1 != null){
+
+        numone = Integer.parseInt(numOne.getText().toString());}
+        if(num2.equals("") && num2 != null){
+        numtwo = Integer.parseInt(numTwo.getText().toString());}
 
         Log.d(LogcatTag, "Successfully grabbed from input fields");
         Log.d(LogcatTag, "numone is: " + numone + " ; " + " numtwo is: " + numtwo);
