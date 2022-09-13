c501d6a5466305d29bb813ce2e4d0690275f4b4d
Yuriy Talabaev <110741536+YuraTal@users.noreply.github.com>
2022-09-13 20:08:16 +0300

    Added go to infActivity and simple open Activity

---
+++ app/src/main/java/ru/synergy/androidstartproj/ComeBeckActivity.java
@@ -0,0 +1,14 @@
+package ru.synergy.androidstartproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComeBeckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_come_beck);
+    }
+}
--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -1,18 +1,74 @@
 package ru.synergy.androidstartproj;
 
+import androidx.activity.OnBackPressedDispatcherOwner;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.Color;
+import android.app.Activity;
+import android.app.UiAutomation;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.ParameterizedType;
+
+public class MainActivity extends Activity implements View.OnClickListener  {
+    private static final int REQ_C = 1;
+    EditText et;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
 
+        et=(EditText) findViewById(R.id.et);
+        tv=(TextView) findViewById(R.id.tv);
+
+        Button btn = (Button) findViewById(R.id.button);
+        Button btn2 = (Button) findViewById(R.id.button2);
+        Button btn3 = (Button) findViewById(R.id.button3);
+
+        btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+
+
     }
+
+
+    @Override
+   public void onClick(View v) {
+       Intent i;
+       switch (v.getId()){
+           case R.id.button:
+               i= new Intent(this,MainActivity2.class);
+               startActivity(i);
+               break;
+           case R.id.button2:
+               i= new Intent(this,ToInfActivity.class);
+               String eText = et.getText().toString();
+               i.putExtra("et", eText);
+               startActivity(i);
+               break;
+//           case R.id.button3:
+//               i = new Intent(this,ComeBeckActivity.class);
+//               startActivityForResult(i, REQ_C);
+       }
+    }
+
+//    @Override
+//    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case RESULT_OK:
+//            tv.setText(data.getStringExtra("et"));
+//        }
+//        return false;
+//    }
+
+
 }
